62b5c99b3cfe088672834523f0023d3e90357437
ekonoval@gmail.com <risk86ing>
2014-05-10 14:43:20 +0300

    add ckeditor to backend root

---
+++ backend/www/ckeditor/config.js
@@ -0,0 +1,25 @@
+﻿/*
+Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http=//ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example=
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+	config.skin = 'v2';
+	
+	CKEDITOR.config.ignoreEmptyParagraph = true;
+	
+	//--- uploader ---//
+/*	
+    filebrowserBrowseUrl = '/ckeditor/_ckfinder/ckfinder.html';
+    filebrowserImageBrowseUrl = '/ckeditor/_ckfinder/ckfinder.html?Type=Images';
+    filebrowserFlashBrowseUrl = '/ckeditor/_ckfinder/ckfinder.html?Type=Flash';
+    filebrowserUploadUrl = '/ckeditor/_ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files';
+    filebrowserImageUploadUrl = '/ckeditor/_ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images';
+    filebrowserFlashUploadUrl = '/ckeditor/_ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash';
+*/
+
+};
