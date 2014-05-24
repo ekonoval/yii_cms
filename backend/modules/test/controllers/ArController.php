5ee369a3975a6223021836bb60894d2828b889e4
ekonoval@gmail.com <risk86ing>
2014-05-24 13:40:33 +0300

    m

--- backend/modules/test/controllers/ArController.php
+++ backend/modules/test/controllers/ArController.php
@@ -2,6 +2,7 @@
 namespace Ekv\B\modules\test\controllers;
 
 use BTestOrderBase;
+use BTestOrderBaseBrowse;
 use BTestOrderExtra;
 use Ekv\B\components\Controllers\BackendControllerBase;
 use Ekv\B\modules\test\controllers\Ar\ArOrderCreateAction;
@@ -73,70 +74,71 @@ class ArController extends BackendControllerBase
     function actionIndex()
     {
         $model = new BTestOrderBase("search");
+
         $model->unsetAttributes();
         $this->assignFormGetAttributes($model);
 
         $this->renderAuto(array('model' => $model));
     }
 
-    function actionUpdate1()
-    {
-        $rowID = yR()->getQuery('idOrder');
-
-        $edit_mode = true;
-
-        /**
-         * @var $modelBase BTestOrderBase
-         */
-        $modelBase = null;
-        $modelExtra = null;
-
-        if (!$edit_mode) {
-//            $model = new \BTransWord();
-//            $model->unsetAttributes();
-        } else {
-            $modelBase = BTestOrderBase::getModelByPk($rowID);
-            $modelExtra = $modelBase->orderExtras;
-
-            //$modelExtra = BTestOrderExtra::model()->findByAttributes(array('baseOrderID' => $rowID));
-        }
-
-        if (!$modelBase) {
-            throw new \CHttpException(404, \Yii::t('StoreModule.admin', 'Incorrect ID'));
-        }
-
-
-        //$form = OrderEditForm::create($modelBase);
-        $form = OrderEditForm::create(null);
-        $form["base"]->model = $modelBase;
-        $form["extra"]->model = $modelExtra;
-
-        //--- check was form posted ---//
-        if($this->isEditFormPostedMulti(array($modelBase, $modelExtra))){
-            if (
-                $modelBase->validate()
-                && $modelExtra->validate()
-            ) {
-                $baseSaveRes = $modelBase->save(false);
-                if($baseSaveRes){
-
-                    $modelExtra->save(false);
-                }
-                $this->setFlashMessage(\Yii::t('StoreModule.admin', 'Изменения успешно сохранены'));
-
-                $this->redirect($this->getIndexUrl());
-            }
-        }else{
-
-        }
-
-        //pa($form->getElements());
-
-        $this->render("update_tpl", array(
-            'model'=>$modelBase,
-            'form'=>$form,
-        ));
-    }
+//    function actionUpdate1()
+//    {
+//        $rowID = yR()->getQuery('idOrder');
+//
+//        $edit_mode = true;
+//
+//        /**
+//         * @var $modelBase BTestOrderBase
+//         */
+//        $modelBase = null;
+//        $modelExtra = null;
+//
+//        if (!$edit_mode) {
+////            $model = new \BTransWord();
+////            $model->unsetAttributes();
+//        } else {
+//            $modelBase = BTestOrderBase::getModelByPk($rowID);
+//            $modelExtra = $modelBase->orderExtras;
+//
+//            //$modelExtra = BTestOrderExtra::model()->findByAttributes(array('baseOrderID' => $rowID));
+//        }
+//
+//        if (!$modelBase) {
+//            throw new \CHttpException(404, \Yii::t('StoreModule.admin', 'Incorrect ID'));
+//        }
+//
+//
+//        //$form = OrderEditForm::create($modelBase);
+//        $form = OrderEditForm::create(null);
+//        $form["base"]->model = $modelBase;
+//        $form["extra"]->model = $modelExtra;
+//
+//        //--- check was form posted ---//
+//        if($this->isEditFormPostedMulti(array($modelBase, $modelExtra))){
+//            if (
+//                $modelBase->validate()
+//                && $modelExtra->validate()
+//            ) {
+//                $baseSaveRes = $modelBase->save(false);
+//                if($baseSaveRes){
+//
+//                    $modelExtra->save(false);
+//                }
+//                $this->setFlashMessage(\Yii::t('StoreModule.admin', 'Изменения успешно сохранены'));
+//
+//                $this->redirect($this->getIndexUrl());
+//            }
+//        }else{
+//
+//        }
+//
+//        //pa($form->getElements());
+//
+//        $this->render("update_tpl", array(
+//            'model'=>$modelBase,
+//            'form'=>$form,
+//        ));
+//    }
 
 }
  
