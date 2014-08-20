714bb8bd09c371d270dfeb96f8cba529b694de8d
ekonoval@gmail.com <risk86ing>
2014-08-20 12:11:53 +0300

    fix SP index sort

--- backend/modules/core/models/staticPage/BStatPage.php
+++ backend/modules/core/models/staticPage/BStatPage.php
@@ -1,6 +1,5 @@
 <?php
 
-use Ekv\components\Yii\Db\ActiveRecordBase;
 use Ekv\models\MPage;
 
 class BStatPage extends MPage
@@ -9,6 +8,27 @@ class BStatPage extends MPage
     {
         return parent::model($className);
     }
+
+    public function search()
+    {
+        $criteria = new CDbCriteria;
+
+        $criteria->compare('idPage', $this->idPage);
+        $criteria->compare('pageTitle', $this->pageTitle, true);
+        $criteria->compare('metaDescr', $this->metaDescr, true);
+        $criteria->compare('metaKeywords', $this->metaKeywords, true);
+        $criteria->compare('dateCreated', $this->dateCreated, true);
+        $criteria->compare('pageBody', $this->pageBody, true);
+        $criteria->compare('pageEnabled', $this->pageEnabled);
+
+        $criteria->order = 't.idPage DESC';
+
+        return new CActiveDataProvider($this, array(
+            'criteria' => $criteria,
+
+        ));
+    }
+
 
 }
  
